c0e09d8247a0d3771aa74ba4adc08d218f5553d2
Altan <suyanovaltan@gmail.com>
2023-06-12 17:51:00 +0300

    first commit

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const mainPage = () => {
+    return (
+        <div className="mainPage">
+            <h3>Номер телефона</h3>
+            <input></input>
+            <h3>Email</h3>
+            <input></input>
+        </div>
+    )
+}
+
+export default mainPage
